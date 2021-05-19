f4f4245ac60a2de4d7967c20708713b3e399708d
Salasc2 <salasch2@gmail.com>
2021-05-18 17:04:02 -0700

    initial setup for backend service

---
+++ src/main/java/CST438Assign3b/Cst438Assign3bApplication.java
@@ -0,0 +1,13 @@
+package CST438Assign3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Assign3bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Assign3bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CST438Assign3b/Cst438Assign3bApplicationTests.java
@@ -0,0 +1,13 @@
+package CST438Assign3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Assign3bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
