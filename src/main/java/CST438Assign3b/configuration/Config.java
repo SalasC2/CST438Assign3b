05f09ded97eb033d24ca4537b9ef0ec24d869bd2
Salasc2 <salasch2@gmail.com>
2021-05-18 17:37:37 -0700

    add logic for backend service to create event listener

---
+++ src/main/java/CST438Assign3b/configuration/Config.java
@@ -0,0 +1,28 @@
+package CST438Assign3b.configuration;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import CST438Assign3b.listeners.ReservationEventHandler;
+
+@Configuration
+public class Config {
+    @Bean
+    public FanoutExchange fanout() {
+        return new FanoutExchange("city-reservation");
+    }
+    @Bean
+    public Queue queue1() {
+        return new Queue("city-reservation-q1");
+    }
+    @Bean
+    public Binding binding1(FanoutExchange fanout, Queue queue1) {
+        return BindingBuilder.bind(queue1).to(fanout);
+    }
+    @Bean
+    public ReservationEventHandler receiver() {
+        return new ReservationEventHandler();
+    }
+}
